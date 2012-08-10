56f785594cba508ba8d8ebbcba52df898a21e1aa
William <wilbefast@gmail.com>
2012-08-10 21:22:26 +0200

    reverted to eb3b6936487c29e0cddeb24fae4525f8214e57d0

--- src/OverheadCamera.hpp
+++ src/OverheadCamera.hpp
@@ -1,3 +1,21 @@
+/*
+Open War: an open-source Total War clone, written in C++ using Ogre3D.
+Copyright (C) 2012 William James Dyce
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef OVERHEADCAMERA_HPP_INCLUDED
 #define OVERHEADCAMERA_HPP_INCLUDED
 
